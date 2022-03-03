492fad9523834358f0c9456ed3f9faa63c336625
sakilshikdar <sakilshikdar2504@gmail.com>
2022-03-02 12:22:18 -1200

    realse date part done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const searchPhone = () => {
     document.getElementById('search-result').innerHTML = '';
-    document.getElementById('phone-details').innerHTML = ''
+    document.getElementById('phone-details').innerHTML = '';
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value;
     searchField.value ='';
@@ -8,70 +8,65 @@ const searchPhone = () => {
     if(searchText == '') {
        document.getElementById('error-message').style.display = 'block'
     }
-    else if(searchText.status == false){
-        document.getElementById('error-message').style.display = 'block'
-    }
     else{
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res => res.json())
-    .then(data =>displayPhone(data.data))
-    }
-    
+    .then(data => displayPhone (data))
+    } 
 }
 
-  const displayPhone = (brand) => { 
-      const data = brand.slice(0, 20);
+const displayPhone = (brand) => { 
+    console.log(brand)
     const searchResult = document.getElementById('search-result')
-   for(const brands of data){
-        const div = document.createElement('div')
-        div.classList.add('col')
-        div.innerHTML = `
-         <img src="${brands.image}" class="w-75 card-img-top" alt="...">
-         <div class="card-body">
-           <h6>Name: ${brands.phone_name}</h6>
-            <h5>Brand: ${brands.brand}</h5>
-         </div>
-         <button onclick='singlePhoen("${brands.slug}")' class="btn btn-primary mb-4 mx-4">Details</button>
-        </div>
-        `
-        searchResult.appendChild(div)
+    if(brand.status == false){
+    document.getElementById('error-message').style.display = 'block'
+     searchResult.innerHTML ='';
     }
+    else{
+        document.getElementById('error-message').style.display = 'none'
+        searchResult.innerHTML ='';
+            const data = brand.data.slice(0, 20); 
+         for(const brands of data){
+            const div = document.createElement('div')
+            // div.classList.add('col')
+            div.innerHTML = `
+            <img src="${brands.image}" class="w-75 card-img-top" alt="...">
+            <div class="card-body">
+              <h6>Name: ${brands.phone_name}</h6>
+               <h5>Brand: ${brands.brand}</h5>
+            </div>
+            <button onclick='singlePhoen("${brands.slug}")' class="btn btn-primary mb-4 mx-4">Details</button>
+            `
+            searchResult.appendChild(div)
+        }   
+    }  
 }
 const singlePhoen = (info) => {
-    // console.log(info)
-    if(!info.status == true){
+    console.log(info)
         const url = `https://openapi.programming-hero.com/api/phone/${info}`
     fetch(url)
     .then(res => res.json())
     .then(data => phoneDetails(data))
-    }
-    else{
-        document.getElementById('error-message').style.display ='block'
-    }
-    document.getElementById('phone-details').innerHTML = ''
+    document.getElementById('phone-details').innerHTML = '';
 }
 
 const phoneDetails =(phoneId) =>{
-    console.log(phoneId)
+    // console.log(phoneId)
     const phoneDiv = document.getElementById('phone-details');
     const div = document.createElement('div')
     div.innerHTML = `
-    <div class="">
-    <div>
     <img class=" rounded mx-auto d-block img-fluid" src="${phoneId.data.image}" class="card-img-top" alt="...">
-    </div>
     <div class="card-body">
     <h6>Name: ${phoneId.data.name}</h6>
-    <h6>ReleasDate: ${phoneId.data.releaseDate}</h6>
+     <h6>ReleasDate:${phoneId.data.releaseDate ? phoneId.data.releaseDate : ' coming soon'}</h6>
       <h5 class="card-title text-primary">mainFeature:</h5>
       <P>ChipSet: ${phoneId.data.mainFeatures.chipSet}</p>
       <P>Displaysize: ${phoneId.data.mainFeatures.displaySize}</p>
       <P>Memory: ${phoneId.data.mainFeatures.memory}</p>
       <h5 class="card-title text-primary">Others:</h5>
-      <p>Bluetooth: ${phoneId.data.others.Bluetooth}</p>
-      <p>USB: ${phoneId.data.others.USB}</p>
-    </div>
+        <p>Bluetooth: ${(phoneId.data.others ) == undefined ?'no info found':phoneId.data.others.Bluetooth}</p>
+        <p>USB: ${(phoneId.data.others ) == undefined ?'no info found':phoneId.data.others.USB}</p>
     </div>
     `
     phoneDiv.appendChild(div)
